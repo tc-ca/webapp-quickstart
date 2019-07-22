6d564cded944688b738aeeea3c0ddae84186106e
Dominic Phillips <dominic.phillips@tc.gc.ca>
2019-07-22 13:56:20 -0400

    Add httpd to setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ export MSYS_NO_PATHCONV=1
 subj="/C=Ca/L=Ottawa/O=Transport Canada/CN=app"
 
 # Setup registry
+echo "Setting up registry"
 cd ./00-registry/
 rm -r certs
 mkdir certs
@@ -11,5 +12,14 @@ openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout domain.key -
 cd ..
 cp .env.example .env
 cd ..
+
+# Setup httpd 
+echo "Setting up httpd"
+cd 01-httpd/etc-httpd/
+rm -r ssl
+mkdir ssl
+cd ssl
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout domain.key -out domain.crt -subj "$subj"
+cd ../../..
 
 
