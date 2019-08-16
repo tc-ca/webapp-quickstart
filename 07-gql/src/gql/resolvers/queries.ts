33a24d52f4220ee97d08424676f1eef74545930d
Dominic Phillips <dominic.phillips@tc.gc.ca>
2019-08-16 14:14:18 -0400

    Update home page

--- 07-gql/src/gql/resolvers/queries.ts
+++ 07-gql/src/gql/resolvers/queries.ts
@@ -23,6 +23,23 @@ export default {
             FROM
                 FC001_TODO
         `);
-    },
-	
+	},
+    async todo(args : any, msg : IncomingMessage, info : GraphQLResolveInfo) {
+        const fields: Dictionary< any > = graphqlFields(info);
+		return (await database.query(`
+			SELECT TOP(1)
+					${
+						[
+							{gqlName: 'id', dbName: 'TODO_ID'},
+							{gqlName: 'name', dbName: 'NAME_NM'},
+							{gqlName: 'priority', dbName: 'PRIORITY_AMT'},
+						]
+							.filter((x:any) => x.gqlName in fields)
+							.map((x: any) => `[${x.dbName}] AS [${x.gqlName}]`)
+							.join(',\n')
+					}
+			FROM
+				FC001_TODO
+		`))[0];
+	},
 };
--- 08-vue/src/views/Home.vue
+++ 08-vue/src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
 	<div>
-		Hello!
+		<h1>{{ title }}</h1>
+		<input v-model="title"/>
+		<br/>
+		<button @click="count++">{{ $t('common.clickCount',{count}) }}</button>
+		<!-- 
+			clickCount: 'You\'ve clicked {{ count }} time',
+			clickCount_plural: 'You\'ve clicked {{ count }} times'
+		 -->
+		<br/>
+		<button @click="count=0">Reset</button>
 	</div>
 </template>
 
@@ -9,8 +18,25 @@ import { wrap } from "../utils";
 
 export default wrap({
 	name: 'Home',
+	data() {
+		return {
+			title: 'Hello world!',
+			count: 12,
+		} ;
+	},
 });
 </script>
 
 <style scoped>
+h1 {
+	color: red;
+}
+input {
+	background-color: white !important;
+	border-style: inset !important;
+}
+button {
+	background-color: white;
+	border-style: outset !important;
+}
 </style>
